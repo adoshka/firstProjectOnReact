a6f8cccfb472299feada9f6ed48ab710090ca7d5
Ado <adogairbekov@gmail.com>
2022-06-09 15:24:43 +0400

    create filters and finished the first web application on react

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,89 +1,3 @@
-
-// import { Component } from 'react';
-
-// import AppInfo from '../app-info/app-info';
-// import SearchPanel from '../search-panel/search-panel';
-// import AppFilter from '../app-filter/app-filter';
-// import EmployeesList from '../employees-list/employees-list';
-// import EmployeesAddForm from '../employees-add-form/employees-add-form';
-
-// import './app.css';
-
-// class App extends Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             data: [
-//                 {name: 'John C.', salary: 800, increase: false, rise: true, id: 1},
-//                 {name: 'Alex M.', salary: 3000, increase: true, rise: false, id: 2},
-//                 {name: 'Carl W.', salary: 5000, increase: false, rise: false, id: 3}
-//             ]
-//         }
-//         this.maxId = 4;
-//     }
-
-//     deleteItem = (id) => {
-//         this.setState(({data}) => {
-//             return {
-//                 data: data.filter(item => item.id !== id)
-//             }
-//         })
-//     }
-
-//     // Да, пока могут добавляться пустые пользователи. Мы это еще исправим
-//     addItem = (name, salary) => {
-//         const newItem = {
-//             name, 
-//             salary,
-//             increase: false,
-//             rise: false,
-//             id: this.maxId++
-//         }
-//         this.setState(({data}) => {
-//             const newArr = [...data, newItem];
-//             return {
-//                 data: newArr
-//             }
-//         });
-//     }
-
-//     onToggleProp = (id, prop) => {
-//         this.setState(({data}) => ({
-//             data: data.map(item => {
-//                 if (item.id === id) {
-//                     return {...item, [prop]: !item[prop]}
-//                 }
-//                 return item;
-//             })
-//         }))
-//     }
-
-//     render() {
-//         const employees = this.state.data.length;
-//         const increased = this.state.data.filter(item => item.increase).length;
-//         return (
-//             <div className="app">
-//                 <AppInfo employees={employees} increased={increased}/>
-    
-//                 <div className="search-panel">
-//                     <SearchPanel/>
-//                     <AppFilter/>
-//                 </div>
-                
-//                 <EmployeesList 
-//                     data={this.state.data}
-//                     onDelete={this.deleteItem}
-//                     onToggleProp={this.onToggleProp}/>
-//                 <EmployeesAddForm onAdd={this.addItem}/>
-//             </div>
-//         );
-//     }
-// }
-
-// export default App;
-
-
-
 import { Component } from 'react';
 
 import AppInfo from '../app-info/app-info';
@@ -102,7 +16,9 @@ class App extends Component {
                 {name: "John S.", salary: 1000, increase: false, rise: true, id: 1},
                 {name: "Ado K.", salary: 3000, increase: true, rise: false, id: 2},
                 {name: "Vasya P.", salary: 7000, increase: false, rise: false, id: 3}
-            ]
+            ],
+            term: '',
+            filter: 'all'
         }
         this.maxId = 4;
     }
@@ -149,22 +65,52 @@ class App extends Component {
         }))
     }
 
+    searchEmp = (items, term) => {
+        if (term.length === 0) {
+            return items;
+        } 
+
+        return items.filter(item => {
+            return item.name.indexOf(term) > -1
+        })
+    }
+
+    onUpdateSearch = (term) => {
+        this.setState({term});
+    }
+
+    filterPost = (items, filter) => {
+        switch (filter) {
+            case 'rise':
+                return items.filter(item => item.rise);
+            case 'moreThen1000':
+                return items.filter(item => item.salary > 1000);
+            default:
+                return items;
+        }
+    }
+
+    onFilterSelect = (filter) => {
+        this.setState({filter});
+    }
+
     render() {
-        const employees = this.state.data.length
-        const increased = this.state.data.filter(item => item.increase).length
-        const {data} = this.state
+        const employees = this.state.data.length;
+        const increased = this.state.data.filter(item => item.increase).length;
+        const {data, term, filter} = this.state;
+        const visibleData = this.filterPost(this.searchEmp(data, term), filter)
         return (
             <div className="app">
                 <AppInfo employees={employees}
                          increased={increased}/>
     
                 <div className="search-panel">
-                    <SearchPanel/>
-                    <AppFilter/>
+                    <SearchPanel onUpdateSearch={this.onUpdateSearch}/>
+                    <AppFilter filter={filter} onFilterSelect={this.onFilterSelect}/>
                 </div>
     
                 <EmployeesList 
-                    data={data}
+                    data={visibleData}
                     onDelete={this.deleteItem}
                     onToggleProp={this.onToggleProp}/>
                     <EmployeesAddForm
